ae65223b353ccdd2fd37a33e06a7883574d6d545
Jason Mei <jason.mei403@gmail.com>
2018-05-27 01:21:52 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(string)
-  answer = []
-  words = string.split(" ")
-  words.each do |word|
-    answer << word.reverse
-  end
-  answer.join(" ")
-end
+# def reverse_each_word(string)
+#   answer = []
+#   words = string.split(" ")
+#   words.each do |word|
+#     answer << word.reverse
+#   end
+#   answer.join(" ")
+# end
 
 def reverse_each_word(string)
   words = string.split(" ")
