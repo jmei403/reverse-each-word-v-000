03f798521a38866fd982f6c7996a0fb1383985c8
Jason Mei <jason.mei403@gmail.com>
2018-05-27 01:20:35 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,8 +7,8 @@ def reverse_each_word(string)
   answer.join(" ")
 end
 
-def reverse_each_word(string)
-  words = string.split(" ")
-  words.map { |word| word.reverse }
-  words.join(" ")
-end
+# def reverse_each_word(string)
+#   words = string.split(" ")
+#   words.map { |word| word.reverse }
+#   words.join(" ")
+# end
